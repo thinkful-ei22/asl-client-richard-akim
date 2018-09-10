bc7234920df2e92b2f51487dc9e78dad5f3f7c4c
Richard Tom <tomrichard14@gmail.com>
2018-09-10 12:59:11 -0700

    finished registration, working on validation

--- src/components/registration/registration-form.js
+++ src/components/registration/registration-form.js
@@ -1,39 +1,104 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {registerUser} from '../../actions/users';
 
 export class RegistrationForm extends React.Component {
   onSubmit(values) {
     console.log(values);
     const {username, name, password} = values;
     const user = {username, name, password};
-    // return this.props
-    //   .dispatch(registerUser(user))
+    return this.props
+      .dispatch(registerUser(user));
     //   .then(() => this.props.dispatch(login(username, password)));
   }
+
+  // validate(values) {
+  //   const errors = {};
+  //   if(!values.username) {
+  //     errors.username = 'Required';
+  //   } else if (values.username.length > 15) {
+  //     errors.username = 'Must be 15 characters or less';
+  //   }
+
+  //   if(!values.password) {
+  //     errors.password = 'Required';
+  //   } else if (values.password.length < 10) {
+  //     errors.password = 'Must be 10 characters or more';
+  //   }
+
+  //   if(!values.name) {
+  //     errors.name = 'Required';
+  //   }
+
+  //   return errors;
+  // }
+
+  // renderField({
+  //   input,
+  //   label,
+  //   type,
+  //   meta: { touched, error, warning }
+  // }) {
+  //   return (
+  //     <div>
+  //       <label>{label}</label>
+  //       <div>
+  //         <input {...input} placeholder={label} type={type} />
+  //         {touched &&
+  //           ((error && <span>{error}</span>) ||
+  //             (warning && <span>{warning}</span>))}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props;
+    
+
     return (
       <form
-        onSubmit={this.props.handleSubmit(values => 
+        onSubmit={handleSubmit(values => 
           this.onSubmit(values))}
       >
-        <label htmlFor="name">Name</label>
+        {/* <label htmlFor="name">Name</label>
         <Field name="name" 
-          component="input" 
+          component={this.renderField}
           type="text" 
         />
         <label htmlFor="username">Username</label>
-        <Field component="input" type="text" name="username" />
+        <Field component={this.renderField} type="text" name="username" label="Username" />
         <label htmlFor="password">Password</label>
-        <Field component="input" type="text" name="password" />
+        <Field component={this.renderField}  type="text" name="password" />
         <label htmlFor="name">Confrim Password</label>
-        <Field component="input" type="text" name="passwordConfirm" />
-        <button type="submit">Register</button>
+        <Field component={this.renderField} type="text" name="passwordConfirm" /> */}
+        <div>
+          <label htmlFor="name">Name</label>
+          <Field name="name" 
+            component="input"
+            type="text" 
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <Field component="input" type="text" name="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field component="input"  type="text" name="password" />
+        </div>
+        <div>
+          <label htmlFor="name">Confrim Password</label>
+          <Field component="input" type="text" name="passwordConfirm" />
+        </div>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>Register</button>
       </form>
     );
   }
 }
 
 export default reduxForm({
-  form: 'registration'
+  form: 'registration',
+  validate: this.validate
 })(RegistrationForm);
 
