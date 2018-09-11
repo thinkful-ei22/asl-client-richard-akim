40fd08d1dfbae784bb6f4748eee143d40ad5ffe2
Akim Desire <adesi006.ng@gmail.com>
2018-09-11 17:17:48 -0400

    prepping to connect records from back to frontend

--- src/components/questions/question-page.js
+++ src/components/questions/question-page.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import requiresLogin from '../requires-login';
-import {connect} from 'react-redux';
-import {fetchQuestion, correctGuess, wrongGuess} from '../../actions/questions';
+import React from "react";
+import requiresLogin from "../requires-login";
+import { connect } from "react-redux";
+import {
+  fetchQuestion,
+  correctGuess,
+  wrongGuess
+} from "../../actions/questions";
 
-export class QuestionPage extends React.Component{
+export class QuestionPage extends React.Component {
   componentDidMount() {
-    this.props.dispatch(fetchQuestion()); 
+    this.props.dispatch(fetchQuestion());
   }
   nextQuestion(e) {
     e.preventDefault();
-    this.props.dispatch(fetchQuestion()); 
+    this.props.dispatch(fetchQuestion());
   }
   onSubmit(e) {
     e.preventDefault();
     const value = this.input.value;
-    value.toLowerCase() === this.props.question.answer ? this.props.dispatch(correctGuess()):this.props.dispatch(wrongGuess());
-    this.input.value = '';
+    value.toLowerCase() === this.props.question.answer
+      ? this.props.dispatch(correctGuess())
+      : this.props.dispatch(wrongGuess());
+    this.input.value = "";
     this.input.focus();
   }
   render() {
     let loading;
     let question;
     let form;
-    if(!this.props.question) {
-      loading = (<div>loading...</div>);
+    if (!this.props.question) {
+      loading = <div>loading...</div>;
     } else {
       question = (
-        <img 
-          src={this.props.question.imageURL} 
+        <img
+          src={this.props.question.imageURL}
           alt={this.props.question.imageDescription}
         />
       );
@@ -35,43 +41,36 @@ export class QuestionPage extends React.Component{
     if (this.props.correct) {
       form = (
         <div>
-          <h3>{`YOU ARE WINNAR! The answer was indeed ${this.props.question.answer.toUpperCase()}`}</h3>
-          <button 
-            onClick={e => this.nextQuestion(e)}
-            className="question-btn"
-          >
+          <h3
+          >{`YOU ARE WINNAR! The answer was indeed ${this.props.question.answer.toUpperCase()}`}</h3>
+          <button onClick={e => this.nextQuestion(e)} className="question-btn">
             <span>Next</span>
           </button>
-        </div>   
+        </div>
       );
     } else if (this.props.wrong) {
       form = (
         <div>
-          <h3>{`Incorrect! The answer was ${this.props.question.answer.toUpperCase()}`}</h3>
-          <button 
-            onClick={e => this.nextQuestion(e)}
-            className="question-btn"
-          >
+          <h3
+          >{`Incorrect! The answer was ${this.props.question.answer.toUpperCase()}`}</h3>
+          <button onClick={e => this.nextQuestion(e)} className="question-btn">
             <span>Next</span>
           </button>
-        </div>   
+        </div>
       );
     } else {
       form = (
-        <form onSubmit={(e) => this.onSubmit(e)}>
-          <input 
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
             type="text"
-            ref={input=> (this.input = input)}
+            ref={input => (this.input = input)}
             required
             className="question-input"
           />
           <div>
-            <button 
-              type="submit"
-              className="question-btn"
-            >
+            <button type="submit" className="question-btn">
               <span>Submit</span>
-            </button> 
+            </button>
           </div>
         </form>
       );
@@ -86,20 +85,19 @@ export class QuestionPage extends React.Component{
             {loading}
             {question}
           </div>
-          <div className="question-response">
-            {form} 
-          </div>
+          <div className="question-response">{form}</div>
         </div>
       </section>
     );
   }
 }
 const mapStateToProps = state => {
-  return ({
+  return {
     question: state.question.data,
     correct: state.question.correctGuess,
-    wrong: state.question.wrongGuess
-  });
+    wrong: state.question.wrongGuess,
+    currentUser: state.auth.currentUser
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(QuestionPage));
