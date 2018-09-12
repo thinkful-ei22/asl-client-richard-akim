a01be505c37f22e975be6e4ff7156d4eab3cef8f
Akim Desire <adesi006.ng@gmail.com>
2018-09-12 17:09:30 -0400

    responsive css WIP

--- src/components/registration/registration-form.js
+++ src/components/registration/registration-form.js
@@ -1,70 +1,65 @@
-import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import {registerUser} from '../../actions/users';
-import {login} from '../../actions/auth';
-import Input from '../input';
-import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
-const usernameLength = length({max: 15});
-const passwordLength = length({min: 10, max: 72});
-const matchesPassword = matches('password');
+import React from "react";
+import { Field, reduxForm, focus } from "redux-form";
+import { registerUser } from "../../actions/users";
+import { login } from "../../actions/auth";
+import Input from "../input";
+import { required, nonEmpty, matches, length, isTrimmed } from "../validators";
+const usernameLength = length({ max: 15 });
+const passwordLength = length({ min: 10, max: 72 });
+const matchesPassword = matches("password");
 
 export class RegistrationForm extends React.Component {
   onSubmit(values) {
-    const {username, name, password} = values;
-    const user = {username, name, password};
-    console.log(user);
+    const { username, name, password } = values;
+    const user = { username, name, password };
     return this.props
       .dispatch(registerUser(user))
       .then(() => this.props.dispatch(login(username, password)));
   }
 
   render() {
-    const {handleSubmit, pristine, submitting} = this.props;
+    const { handleSubmit, pristine, submitting } = this.props;
 
     return (
-      <form
-        onSubmit={handleSubmit(values => 
-          this.onSubmit(values))}
-      >
+      <form onSubmit={handleSubmit(values => this.onSubmit(values))}>
         <label htmlFor="name">Name</label>
-        <Field name="name" 
-          component={Input} 
-          type="text" 
-          validate={[required]} 
+        <Field
+          name="name"
+          component={Input}
+          type="text"
+          validate={[required]}
         />
         <label htmlFor="username">Username</label>
-        <Field 
+        <Field
           component={Input}
-          type="text" 
-          name="username" 
+          type="text"
+          name="username"
           validate={[required, nonEmpty, isTrimmed, usernameLength]}
         />
         <label htmlFor="password">Password</label>
-        <Field 
+        <Field
           component={Input}
-          type="text" 
-          name="password" 
+          type="text"
+          name="password"
           validate={[required, passwordLength, isTrimmed]}
         />
         <label htmlFor="name">Confrim Password</label>
-        <Field 
-          component={Input} 
-          type="text" 
-          name="passwordConfirm" 
-          validate={[required, nonEmpty, matchesPassword]} 
+        <Field
+          component={Input}
+          type="text"
+          name="passwordConfirm"
+          validate={[required, nonEmpty, matchesPassword]}
         />
-        <button 
-          type="submit"
-          disabled={pristine || submitting}
-        >Register</button>
+        <button type="submit" disabled={pristine || submitting}>
+          Register
+        </button>
       </form>
     );
   }
 }
 
 export default reduxForm({
-  form: 'registration',
+  form: "registration",
   onSubmitFail: (errors, dispatch) =>
-    dispatch(focus('registration', Object.keys(errors)[0]))
+    dispatch(focus("registration", Object.keys(errors)[0]))
 })(RegistrationForm);
-
